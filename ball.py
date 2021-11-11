1d43a7f944500dd7b22127e2d575f5cb79b61f99
onehana1 <73771420+onehana1@users.noreply.github.com>
2021-11-11 14:59:02 +0900

    13

---
+++ ball.py
@@ -0,0 +1,41 @@
+import game_framework
+from pico2d import *
+import game_world
+
+# from boy import Boy
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER) #picel per second
+
+# Boy Action Speed
+# fill expressions correctly
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+
+
+class Ball:
+    image = None
+
+    def __init__(self, x = 400, y = 300, velocity = 1):
+        if Ball.image == None:
+            Ball.image = load_image('ball21x21.png')
+        self.x, self.y, self.velocity = x, y, velocity
+
+    # def do(self):
+    #     boy = Boy
+    #     boy.frame = (boy.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 8
+
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+    def update(self):
+        self.x += self.velocity
+
+        if self.x < 25 or self.x > 1600 - 25:
+            game_world.remove_object(self)
